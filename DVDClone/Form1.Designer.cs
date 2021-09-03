a022c98a54d8f6dd85c8d2132d1c47c2104e2763
shachi <52762781+shachy@users.noreply.github.com>
2021-09-03 23:43:33 +0300

    Add files via upload

---
+++ DVDClone/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace DVDClone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(474, 263);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1254, 681);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DVDClone/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVDClone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int[] pDirection = new int[] { 6, 2 };
+        public int[] VHSpeed = Physics.SpeedRandomizer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point pLocation = panel1.Location;
+
+            // Every 10ms the panel is moving by VHSpeed[X, Y]/20 
+            if(pDirection[0] == 6 && pDirection[1] == 2)
+            {
+                panel1.Location = new Point(pLocation.X + (VHSpeed[0] / 20), pLocation.Y + (VHSpeed[1] / 20));
+                panel1.BackColor = Color.Gold;
+            }
+            else if(pDirection[0] == 6 && pDirection[1] == 8)
+            {
+                panel1.Location = new Point(pLocation.X + (VHSpeed[0] / 20), pLocation.Y - (VHSpeed[1] / 20));
+                panel1.BackColor = Color.CadetBlue;
+            }
+            else if (pDirection[0] == 4 && pDirection[1] == 2)
+            {
+                panel1.Location = new Point(pLocation.X - (VHSpeed[0] / 20), pLocation.Y + (VHSpeed[1] / 20));
+                panel1.BackColor = Color.FloralWhite;
+            }
+            else if (pDirection[0] == 4 && pDirection[1] == 8)
+            {
+                panel1.Location = new Point(pLocation.X - (VHSpeed[0] / 20), pLocation.Y - (VHSpeed[1] / 20));
+                panel1.BackColor = Color.DeepPink;
+            }
+
+            // pDirection[X, Y] 2 DOWN, 8 UP, 4 LEFT, 6 RIGHT
+            int[] PNCLocation = new int[] { pLocation.X, pLocation.Y };
+            if (PNCLocation[1] <= 0)
+            {
+                pDirection[1] = 2;
+            }
+            else if(PNCLocation[1] + 145 >= 720)
+            {
+                pDirection[1] = 8;
+            }
+            else if(PNCLocation[0] + 220 >= 1270)
+            {
+                pDirection[0] = 4;
+            }
+            else if(PNCLocation[0] <= 0)
+            {
+                pDirection[0] = 6;
+            }
+        }
+    }
+
+    public class Physics
+    {
+        public static int[] SpeedRandomizer()
+        {
+            var rand = new Random(Convert.ToInt32(long.Parse(DateTime.UtcNow.ToString("ffffff"))));
+            int[] VHSpeed = new int[] { rand.Next(50, 110), rand.Next(50, 90) };
+            return VHSpeed;
+        }
+    }
+}
